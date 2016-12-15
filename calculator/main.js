0ca82a3b6fc15775355f508d3c186452762d7a3d
Rafique Mahroof <mrgeekee@rafgeekee.com>
2016-12-15 14:09:33 +0000

    clear-button

--- calculator/main.js
+++ calculator/main.js
@@ -4,7 +4,7 @@ var decimalAdded = false;
 
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
-		var input = document.querySelector('.screen');
+		var input = document.querySelector('.display');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 
